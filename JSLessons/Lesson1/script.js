5ebde7a3fa9c24762f12f5d7d790c00e8382e83e
Hasan <hasan.doctype@gmail.com>
2021-08-03 20:44:47 +0400

    add JS Lesson1

---
+++ JSLessons/Lesson1/script.js
@@ -0,0 +1,23 @@
+// one line comment
+
+/*more line comment
+document.write('Hello World');
+*/
+// debugger;
+// document.write("Hello JS"+"<br>");
+// document.write("Hello JS"+"<br>");
+// document.write("Hello JS"+"<br>");
+// document.write("Hello JS"+"<br>");
+// document.write("Hello JS"+"<br>");
+// document.write("Hello JS"+"<br>");
+// document.write("Hello JS"+"<br>");
+// document.write("Hello JS"+"<br>");
+
+// console.log('Hello Front');
+
+// alert("Hello");
+// window.alert("Hello1");
+
+function capital(){
+    document.getElementById("test").innerHTML = "Baku";
+}
