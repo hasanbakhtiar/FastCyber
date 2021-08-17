c17580fec1493e077fee9ad6e0916cb85af985e4
Hasan <hasan.doctype@gmail.com>
2021-08-17 20:03:10 +0400

    add lesson3

---
+++ JSLessons/Lesson3/script.js
@@ -0,0 +1,92 @@
+// All Data Type 
+// --------------------------------------------------------------------------------------------------
+// Primitive (Bəsit)  Data(Data) Type(Tipi) Start
+var infomationText1   = "Salam1";     //String(mətn)      data type ("Hi")
+var infomationText2   = 'Salam2';     //String(mətn)      data type ("Hi")
+var infomationNumb    = 5 ;           //Number(rəqəm)     data type (5)
+var infomationBoolean = true ;        //Boolean(məntiqi)  data type (true/flase)
+
+document.write(infomationText1     + "<br>");
+document.write(infomationText2     + "<br>");
+document.write(infomationNumb      + "10" + "<br>");
+document.write(infomationBoolean   + "<br>");
+// Basic(Bəsit)        Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Composite(Birləşik) Data(Data) Type(Tipi) Start
+var infomationArray  =  ['pc',5,true,'table'];    //Array
+var infomationObject =  {
+    name:"Samsung",
+    ROM: "OneUI"
+};                                                //Object
+
+document.write(infomationArray          + "<br>");
+document.write(infomationObject.name    + "<br>");
+document.write(infomationObject.ROM     + "<br>");
+// Composite(Birləşik) Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Special(Xüsusi)     Data(Data) Type(Tipi) Start
+var informationUndefined = "";   //Undefined
+var informationNULL      = null; //Null
+
+document.write(informationUndefined          + "<br>");
+document.write(informationNULL               + "<br>");
+// Special(Xüsusi)     Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+// Basic Data Type
+        // String
+var infoData = 'Hello FrontEnd';
+var infoNumber = 5.1;
+
+
+var infoNumber1 = '10';
+var infoNumber2 = 5;
+// console.log(infoNumber1+infoNumber2);
+// console.log(infoData);
+// console.log(infoNumber);
+        //Number
+        // Boolean
+        // true
+        // false
+
+    // if (true) {
+    //     console.log(true);
+    // }else{
+    //     console.log(false);
+    // }
+
+
+// Composite Data Type
+        // Array
+        // var infoArray = [1,'Rahide',true];
+        // console.log(infoArray);
+        // var infoArray = new Array();
+        // Object
+        // var infoObj = {
+        //     name: 'Rahide',
+        //     yas:'20'
+        // }
+
+        // console.log(infoObj);
+        // document.write(infoObj.yas)
+     
+
+// Special Data Type    
+        // Undefined
+        // var a = 
+        var a = null;
+console.log(a);
+        // Null
