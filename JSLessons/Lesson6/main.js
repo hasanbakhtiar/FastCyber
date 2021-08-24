2d2eb34eff19f391e59682446bd352a5c9f7ee78
Hasan <hasan.doctype@gmail.com>
2021-08-24 20:09:38 +0400

    add lesson js

---
+++ JSLessons/Lesson6/main.js
@@ -0,0 +1,47 @@
+// index dəyər 0dan başlanır.
+// let a = new Array();
+// let schoolThing = ["table","desk","pen","marker"];
+
+// console.log(schoolThing);
+// console.log(schoolThing[1]);
+
+// schoolThing[0] = "Book";
+// console.log(schoolThing);
+
+// schoolThing['info'] = "Book";
+// console.log(schoolThing['info']);
+let a = "How a u";
+// console.log(a.length);
+
+
+
+
+// let  schoolThing = [];
+// schoolThing[0] = "Book1";
+// schoolThing[1] = "Book2";
+// schoolThing[2] = "Book3";
+// schoolThing[3] = "Book4";
+// schoolThing[4] = "Book5";
+// schoolThing[5] = "Book6";
+// console.log(schoolThing);
+
+// let mixContent = ['table','pen',[1,2,3,4,['snow','sunny',true,100],5,6,7],'desk','book']
+
+// console.log(mixContent[2][4][1]);
+
+
+// let schoolThing = {
+//     thing1: "table",
+//     thing2:"pen",
+//     break: true,
+//     lessonHour: (a)=>{
+//        if (a == 40) {
+//            return a-10;
+//        }else{
+//            return 'error';
+//        }
+//     }
+// }
+
+// console.log(schoolThing.lessonHour(40));
+
---
+++ JSLessons/Lesson8/main.js
@@ -0,0 +1,55 @@
+// const infoNumb = 0;
+
+// switch (infoNumb) {
+//     case 5:
+//         document.write("this is 5");
+//         break;
+
+//         case 6:
+//             document.write("this is 6");
+//             break;
+
+//             case 7:
+//                 document.write("this is 7");
+//                 break;
+
+//                 case 10:
+//                     document.write("this is 10");
+//                     break;
+//                     case "JavaScript":
+//                         document.write("this is JS");
+//                         break;
+
+//     default:
+//         document.write("empty");
+        
+//         break;
+// }
+
+// ------------------------------------------------------------------------------------
+// try {
+//     documedfhfahjklnt.write("This progress is True");
+    
+// throw("bu bir istisnadir");
+    
+// } catch (err) {
+//     document.write("This progress is not correct"+"<br>");
+//     document.write(err);
+    
+// }finally{
+//     document.write("<br>"+"her halda isleyir");
+// }
+
+// try{
+//     throw("this is exception");
+
+//     document.writesdsd("Hello Front");
+
+// }catch(error){
+//     document.write(error);
+//     document.write("<br>have a problem")
+// }finally{
+//     document.write("<br>Every time runing...")
+// }
+
+// ------------------------------------------------------------------------------------
