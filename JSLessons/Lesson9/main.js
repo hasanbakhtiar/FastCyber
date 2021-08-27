5ad6ab4706c98250d0dfd59f9be58966cf91fe7d
Hasan <hasan.doctype@gmail.com>
2021-08-27 20:18:21 +0400

    function

---
+++ JSLessons/Lesson9/main.js
@@ -0,0 +1,28 @@
+// // ES5 
+// function infoFunc(){
+//     // return 5;
+//     console.log(5);
+// };
+
+// let infoFunc1 = function(){
+//     console.log(10);
+// };
+
+//  function infoFunc(a=10,b=20){
+//      console.log(a+b);
+//  }
+
+
+//  infoFunc(1,2);
+
+(function infoFunc(a){
+    console.log(10+a);
+})(5)
+
+
+
+
+
+// console.log(infoFunc());
+// infoFunc();
+// infoFunc1();
