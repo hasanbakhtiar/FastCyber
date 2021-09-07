e65e4a4c77f43bc0d051ecab2029f609ea168202
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2021-09-07 19:50:13 +0400

    add DOM end Lesson

---
+++ JSLessons/Lesson11/main.js
@@ -0,0 +1,11 @@
+// https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_nav_innerhtml2
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_document
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_nav_nodename
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_nav_nodetype
+
+// ----------------------------------------------------------------------------------
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_elementcreate
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_elementcreate2
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_elementremovechild
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_elementreplace
