409852a4ef1fbfcdf8a51d8caae3918dcf1a9146
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2021-09-10 19:53:35 +0400

    bom lessons

---
+++ JSLessons/BOM/main.js
@@ -0,0 +1,17 @@
+// https://www.w3schools.com/js/js_window.asp
+
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_win_inner
+
+// https://www.w3schools.com/js/js_window_screen.asp
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+
+// https://www.w3schools.com/js/js_window_location.asp
+
+// https://www.w3schools.com/js/js_window_history.asp
+
+// https://www.w3schools.com/js/js_window_navigator.asp
+
+// https://www.w3schools.com/js/js_popup.asp
+
+// https://www.w3schools.com/js/js_timing.asp
